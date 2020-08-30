5d420088e32699f5638a918c4b79d8e928363d74
teklithuania <61139751+teklithuania@users.noreply.github.com>
2020-08-30 19:22:34 +0300

    Hero logic

--- js/layout/hero.js
+++ js/layout/hero.js
@@ -1,1 +1,27 @@
 "use strict";
+
+function heroRender() {
+    let HTML = '';
+
+    HTML = `<div class="row" id="hero-row">
+                <div class="my-name">
+                    <h1>
+                        Thad<span>deus</span>
+                    </h1>
+                </div>
+                <div class="header-caption" id="page-top">
+                    <!-- Headline start-->
+                    <h3 class="cd-headline clip is-full-width">
+                        <span>I'm </span>
+                        <span class="cd-words-wrapper" style="width: 178px;">
+                        <b class="is-visible">Developer.</b>
+                        <b class="is-hidden"> Designer.</b>
+                        <b class="is-hidden"> Freelancer.</b>
+                        </span>
+                    </h3>
+                    <!-- Headline end -->
+                </div>
+            </div>`;
+    document.querySelector('#hero-row').innerHTML = HTML;
+}
+export default heroRender;
--- js/main.js
+++ js/main.js
@@ -5,13 +5,14 @@ import renderHeader from './layout/header.js';
 import scrollFun from './layout/scroll.js';
 import headerBackground from './layout/showSideMenu.js';
 import wordSlider from './layout/wordSlider.js';
-// import heroSection from './layout/hero.js';
+import heroRender from './layout/hero.js';
 
 // create
 // new callScrollFun('#main_header');
 
 // call up function
 renderHeader(header);
+heroRender();
 scrollFun();
 headerBackground();
 wordSlider();

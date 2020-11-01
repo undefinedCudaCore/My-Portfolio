6e78b385719016f0fa5967ebd57052fc762f68dc
teklithuania <61139751+teklithuania@users.noreply.github.com>
2020-11-01 19:02:59 +0200

    Portfolio data update

--- js/data/portfolioData.js
+++ js/data/portfolioData.js
@@ -18,6 +18,24 @@ const portfolio = [
         src: `./img/portfolio/mim.png`,
         alt: `Mim`,
         paragraph: `Mim`
+    },
+    {
+        link: `https://github.com/undefinedCudaCore/PHP-HomeWork/tree/master/bankas2`,
+        src: `./img/portfolio/bank.png`,
+        alt: `Bank`,
+        paragraph: `Bank`
+    },
+    {
+        link: `https://github.com/undefinedCudaCore/restik`,
+        src: `./img/portfolio/restik.png`,
+        alt: `Grill`,
+        paragraph: `Grill`
+    },
+    {
+        link: `https://front-end-by-rimantas.github.io/14-grupe-portfolio-mim/`,
+        src: `./img/portfolio/gradebook.png`,
+        alt: `Grade`,
+        paragraph: `Grade`
     }
 ];
 
